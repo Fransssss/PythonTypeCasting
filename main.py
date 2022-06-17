c1060a30b6b1bd991f554009dc8b2911650b5c08
Frans <97081479+Fransssss@users.noreply.github.com>
2022-06-16 18:26:15 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+#Type Casting
+
+print("\n==Type Casing==\n")
+numAsInt = 20
+numAsFloat = 30.2
+numAsStr = "100"
+
+print(numAsInt)
+print(numAsFloat)
+print(numAsStr)
+print('\n')
+
+print("-Cast to int")
+numfloat = 10.1
+print(numfloat)
+numfloat1 = 20.2
+print(numfloat1)
+numfloat2 = 30.3
+print(numfloat2)
+numfloat3 = 40.0
+print(numfloat3)
+print('\n')
+
+numInt = 1;
+numFlt = 2.0
+numStr = "3"
+
+print(numInt)
+print(type(numInt))
+print(numFlt)
+print(type(numFlt))
+print(numStr)
+print(type(numStr))
+
+print("\nConvert to Int")
+numInt = int(numInt)
+numFlt = int(numFlt)
+numStr = int(numStr)
+
+print(numInt)
+print(type(numInt))
+print(numFlt)
+print(type(numInt))
+print(numStr)
+print(type(numStr))
+
+print("\nConvert to float")
+numInt = float(numInt)
+numFlt = float(numFlt)
+numStr = float(numStr)
+
+print(numInt)
+print(type(numInt))
+print(numFlt)
+print(type(numFlt))
+print(numStr)
+print(type(numStr))
+
+print("\nConvert to str")
+numInt = str(numInt)
+numFlt = str(numFlt)
+numStr = str(numStr)
+
+print(numInt)
+print(type(numInt))
+print(numFlt)
+print(type(numFlt))
+print(numStr)
+print(type(numStr))
+
